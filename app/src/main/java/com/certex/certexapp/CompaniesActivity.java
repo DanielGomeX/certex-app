c873fe469918f56043230d69acea6a79722bbd75
jackson <jackson_wie@hotmail.com>
2019-05-22 13:37:17 -0300

    Criado Tela

---
+++ app/src/main/java/com/certex/certexapp/CompaniesActivity.java
@@ -0,0 +1,21 @@
+package com.certex.certexapp;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CompaniesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_companies);
+    }
+
+//    @Override
+//    public void onBackPressed() {
+//        setContentView(R.layout.activity_companies);
+//    }
+}
--- app/src/main/java/com/certex/certexapp/MainActivity.java
+++ app/src/main/java/com/certex/certexapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.certex.certexapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt_login = (Button) findViewById(R.id.btn_login);
+
+        bt_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_companies);
+            }
+        });
     }
-    //sincronizou?
+    //sincronizou? SIM
 }
